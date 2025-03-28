8daecf1643b29cc56f7a99e10f3549a138d75f73
dsaikiran01 <dsaikiran559@gmail.com>
2025-03-28 08:49:41 +0530

    changed absolute to relative paths

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,7 +9,7 @@ export default function Home() {
 
         <Image
           className="dark:invert"
-          src="/next.svg"
+          src="./next.svg"
           alt="Next.js logo"
           width={180}
           height={38}
@@ -37,7 +37,7 @@ export default function Home() {
           >
             <Image
               className="dark:invert"
-              src="/vercel.svg"
+              src="./vercel.svg"
               alt="Vercel logomark"
               width={20}
               height={20}
@@ -78,7 +78,7 @@ export default function Home() {
         >
           <Image
             aria-hidden
-            src="/window.svg"
+            src="./window.svg"
             alt="Window icon"
             width={16}
             height={16}
@@ -93,7 +93,7 @@ export default function Home() {
         >
           <Image
             aria-hidden
-            src="/globe.svg"
+            src="./globe.svg"
             alt="Globe icon"
             width={16}
             height={16}
